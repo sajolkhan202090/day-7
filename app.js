acdd8b42926fb3fe221141f4ae29fe9a980f5411
sajolkhan202090 <sajolkhan105@gmail.com>
2022-04-28 01:43:37 +0600

    file uplode

---
+++ app.js
@@ -0,0 +1,386 @@
+
+
+const studenInfo = [
+    {
+        name:'sajol',
+        roll: 1,
+        age: 19,
+        class: 5,
+        gender:'male',
+        location:'pabna',
+        admission_fee:2000,
+        
+    },
+    {
+        name:'akash',
+        roll: 2,
+        age: 18,
+        class: 6,
+        gender:'male',
+        location:'pabna',
+        admission_fee:1000,
+    },
+    {
+        name:'taher',
+        roll: 3,
+        age: 19,
+        class: 7,
+        gender:'male',
+        location:'khulna',
+        admission_fee:1000,
+    },
+    {
+        name:'subbir',
+        roll: 3,
+        age: 19,
+        class: 8,
+        gender:'male',
+        location:'dhaka',
+        admission_fee:3000,
+    },
+    {
+        name:'anish',
+        roll: 4,
+        age: 19,
+        class: 9,
+        gender:'male',
+        location:'mirpur',
+        admission_fee:4000,
+        
+    },
+    {
+        name:'sufia',
+        roll: 5,
+        age: 20,
+        class: 9,
+        gender:'female',
+        location:'banani',
+        admission_fee:6000,
+    },
+    {
+        name:'aisha',
+        roll: 9,
+        age: 21,
+        class: 10,
+        gender:'female',
+        location:'khulna',
+        admission_fee:7000,
+    },
+    {
+        name:'najmul',
+        roll: 10,
+        age: 22,
+        class: 5,
+        gender:'male',
+        location:'dhaka',
+        admission_fee:3000,
+    },
+    {
+        name:'shohid',
+        roll: 11,
+        age: 23,
+        class: 6,
+        gender:'male',
+        location:'mirpur',
+        admission_fee:8000,
+        
+    },
+    {
+        name:'nahid',
+        roll: 12,
+        age: 24,
+        class: 7,
+        gender:'male',
+        location:'banani',
+        admission_fee:9000,
+    },
+    {
+        name:'tamima',
+        roll: 1,
+        age: 19,
+        class: 10,
+        gender:'female',
+        location:'khulna',
+        admission_fee:10000,
+    },
+    {
+        name:'jabin',
+        roll: 2,
+        age: 19,
+        class: 10,
+        gender:'female',
+        location:'khulna',
+        admission_fee:3000,
+    },
+    {
+        name:'habiba',
+        roll: 5,
+        age: 20,
+        class: 6,
+        gender:'female',
+        location:'dhaka',
+        admission_fee:5000,
+        
+    },
+    {
+        name:'nayem',
+        roll: 9,
+        age: 24,
+        class: 9,
+        gender:'male',
+        location:'mirpur',
+        admission_fee:9000,
+    },
+    {
+        name:'ibrahim',
+        roll: 8,
+        age: 22,
+        class: 9,
+        gender:'male',
+        location:'khulna',
+        admission_fee:1000,
+    },
+    {
+        name:'munire',
+        roll: 3,
+        age: 25,
+        class: 8,
+        gender:'male',
+        location:'banani',
+        admission_fee:3000,
+    },
+    {
+        name:'yeasin',
+        roll: 1,
+        age: 18,
+        class: 8,
+        gender:'male',
+        location:'khulna',
+        admission_fee:2000,
+        
+    },
+    {
+        name:'farhad',
+        roll: 2,
+        age: 22,
+        class: 6,
+        gender:'male',
+        location:'dhaka',
+        admission_fee:1000,
+    },
+    {
+        name:'tohidul',
+        roll: 3,
+        age: 19,
+        class: 7,
+        gender:'male',
+        location:'mirpur',
+        admission_fee:5000,
+    },
+    {
+        name:'shahin',
+        roll: 6,
+        age: 19,
+        class: 9,
+        gender:'male',
+        location:'dhaka',
+        admission_fee:3000,
+    },
+    {
+        name:'fariad',
+        roll: 1,
+        age: 15,
+        class: 5,
+        gender:'male',
+        location:'khulna',
+        admission_fee:2000,
+        
+    },
+    {
+        name:'sagor',
+        roll: 5,
+        age: 25,
+        class: 6,
+        gender:'male',
+        location:'mirpur',
+        admission_fee:11000,
+    },
+    {
+        name:'rasel',
+        roll: 3,
+        age: 19,
+        class: 7,
+        gender:'male',
+        location:'banani',
+        admission_fee:1000,
+    },
+    {
+        name:'nasim',
+        roll: 3,
+        age: 19,
+        class: 8,
+        gender:'male',
+        location:'khulna',
+        admission_fee:3000,
+    },
+    {
+        name:'sajol',
+        roll: 1,
+        age: 19,
+        class: 5,
+        gender:'male',
+        location:'pabna',
+        admission_fee:2000,
+        
+    },
+    {
+        name:'akash',
+        roll: 2,
+        age: 18,
+        class: 6,
+        gender:'male',
+        location:'pabna',
+        admission_fee:1000,
+    },
+    {
+        name:'taher',
+        roll: 3,
+        age: 19,
+        class: 7,
+        gender:'male',
+        location:'khulna',
+        admission_fee:1000,
+    },
+    {
+        name:'subbir',
+        roll: 3,
+        age: 19,
+        class: 8,
+        gender:'male',
+        location:'dhaka',
+        admission_fee:3000,
+    },
+    {
+        name:'tarek',
+        roll: 6,
+        age: 24,
+        class: 5,
+        gender:'male',
+        location:'mirpur',
+        admission_fee:2000,
+        
+    },
+    {
+        name:'saiful',
+        roll: 8,
+        age: 22,
+        class: 9,
+        gender:'male',
+        location:'banani',
+        admission_fee:5000,
+    },
+    {
+        name:'salman',
+        roll: 5,
+        age: 24,
+        class: 7,
+        gender:'male',
+        location:'khulna',
+        admission_fee:9000,
+    },
+    
+];
+
+
+
+// Total Admission Fees
+
+
+// let total_fees = 0;
+// for(data of studenInfo){
+//     console.log(`
+//     my name : ${data.name}
+//     roll   : ${data.roll}
+//     age     :${data.age}
+//     class   : ${data.class}
+//     gendar  : ${data.gender}
+//     location : ${data.location}
+//     admission_fee : ${data.admission_fee}
+//     `);
+//     total_fees+=data.admission_fee
+// }
+
+// console.log(`
+// -------------------------------------------
+// total amoun       ${total_fees}
+// `);
+
+
+
+
+// all find femal 
+
+
+// let total_female = 0;
+// studenInfo.forEach((gendar) =>{
+//     if(gendar.gender==='female'){
+
+//         console.log(`
+//         name : ${gendar.name}
+//         roll : ${gendar.roll}
+//         class : ${gendar.class}
+//         gendar : ${gendar.gender}
+//         location : ${gendar.name}
+        
+//         `);
+//         total_female++
+//     }
+// });
+
+// console.log(`
+// ----------------------------->
+// total     ${total_female}
+// `);
+
+
+// find all location
+
+// studenInfo.forEach((data)=>{
+//     if(data.location==='khulna'){
+//         console.log(`
+//         name : ${data.name}
+//         roll : ${data.roll}
+//         class : ${data.class}
+//         location: ${data.location}
+//         `);
+//     }
+// })
+
+
+// find_class_five
+
+// studenInfo.forEach((data)=>{
+//     if(data.class == 5){
+//         console.log(`
+        // name : ${data.name}
+        // roll : ${data.roll}
+        // class : ${data.class}
+        
+//         `);
+//     }
+// })
+
+
+// all find age
+
+studenInfo.forEach((age) => {
+    if(age.age >= 10 && age.age <= 25 ){
+        console.log(`
+        name : ${age.name}
+        roll : ${age.roll}
+        class : ${age.class}
+        age : ${age.age}
+        `);
+    }
+
+})
